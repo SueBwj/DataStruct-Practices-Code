e7c918ef32d0d51639f5716883dedb047aa59858
Sue <1175634156@qq.com>
2023-10-11 08:22:58 +0800

    finish Queue_Insert function

--- data structure/Tree/CBT_Tree.cpp
+++ data structure/Tree/CBT_Tree.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+#include <queue>
 using namespace std;
 
 // 用数组构成CBT，i为父节点，则2*i+1为左子节点，2*i+2为右子节点
@@ -30,6 +32,40 @@ Node *Recursion_Insert(const int *arr, int i, int n)
     return root;
 }
 
+// queue insert
+Node *Queue_Insert(vector<int> arr)
+{
+    if (arr.size() == 0)
+    {
+        return nullptr;
+    }
+    else
+    {
+        Node *root = new Node(arr[0]);
+        queue<Node *> q;
+        q.push(root);
+        int i = 1;
+        while (i < arr.size())
+        {
+            Node *cur = q.front();
+            q.pop();
+            if (i < arr.size())
+            {
+                Node *node = new Node(arr[i++]);
+                cur->left = node;
+                q.push(node);
+            }
+            if (i < arr.size())
+            {
+                Node *node = new Node(arr[i++]);
+                cur->right = node;
+                q.push(node);
+            }
+        }
+        return root;
+    }
+}
+
 // Function to print tree nodes in
 // InOrder fashion
 void inOrder(Node *root)
@@ -45,8 +81,7 @@ void inOrder(Node *root)
 // Driver program to test above function
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 6, 6, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    Node *root = Recursion_Insert(arr, 0, n);
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 6, 6, 6};
+    Node *root = Queue_Insert(arr);
     inOrder(root);
 }
